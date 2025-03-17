0e09b44344a95200a56cbc410e02edb513074c66
Tobias Rødahl Thingnes <tobbi.rt@gmail.com>
2025-03-17 14:48:44 +0100

    Baseline dashboard

--- src/main/webui/src/App.tsx
+++ src/main/webui/src/App.tsx
@@ -1,20 +1,35 @@
-import { useGetApiReport } from "../openapi/queries"
+import { useGetApiReport, usePostApiCheck } from "../openapi/queries"
 import "./App.css"
 
 function App() {
-    const { data, isLoading, isError } = useGetApiReport();
+    const { data, isLoading, isError, refetch } = useGetApiReport()
+    const { mutateAsync } = usePostApiCheck()
 
     return (
         <>
             <h1>Cybersecurity Dashboard</h1>
+            <button
+                onClick={async () => {
+                    await mutateAsync({})
+                    refetch()
+                }}
+            >
+                Check Now
+            </button>
             <div className="card">
                 {isLoading && "Loading..."}
                 {isError && "Error!"}
-                {data && (
+                {data?.results && (
                     <div>
-                        { data.results.map((result) => <p key={result.check.name}>
-                            {result.check.name} reported risk {result.risk} {result.message ? `with message "${result.message}"` : ""}
-                        </p>)}
+                        <b>Last updated {new Date(data.timestamp).toLocaleString()}</b>
+                        {data.results.map((result) => (
+                            <p key={result.check.name}>
+                                {result.check.name} reported risk {result.risk}{" "}
+                                {result.message
+                                    ? `with message "${result.message}"`
+                                    : ""}
+                            </p>
+                        ))}
                     </div>
                 )}
             </div>
