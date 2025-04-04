3d2582910f29fb22385665059f823ebea646d7a7
Tobias Rødahl Thingnes <tobbi.rt@gmail.com>
2025-04-04 10:45:24 +0200

    Add all potential tabs and * path for non-existing

--- addon/src/main/webui/src/App.tsx
+++ addon/src/main/webui/src/App.tsx
@@ -1,3 +1,10 @@
+import {
+    Checklist,
+    Description,
+    LocalLibrary,
+    LockOpen,
+    People,
+} from "@mui/icons-material"
 import { Box, Grid, Tab, Tabs } from "@mui/material"
 import { Outlet, useLocation, useNavigate } from "react-router"
 
@@ -5,7 +12,7 @@ function App() {
     const navigate = useNavigate()
     const { pathname } = useLocation()
     return (
-        <Grid container m={2}>
+        <Grid container m={1}>
             <Grid size="grow"></Grid>
             <Grid size={{ xs: 12, sm: 10, md: 8, xl: 6 }} justifyItems="center">
                 <Tabs
@@ -13,10 +20,17 @@ function App() {
                     value={pathname}
                     onChange={(_, v) => navigate(v)}
                 >
-                    <Tab label="Common Risk Checks" value="/" />
-                    <Tab label="Data Overview" value="/analysis" />
+                    <Tab label="Overview" icon={<Checklist />} value="/" />
+                    <Tab label="Logs" icon={<Description />} value="/logs" />
+                    <Tab label="Access" icon={<LockOpen />} value="/access" />
+                    <Tab
+                        label="Community"
+                        icon={<People />}
+                        value="/community"
+                    />
+                    <Tab label="Learn" icon={<LocalLibrary />} value="/learn" />
                 </Tabs>
-                <Box mt={5}>
+                <Box mt={3}>
                     <Outlet />
                 </Box>
             </Grid>
--- addon/src/main/webui/src/AppRoutes.tsx
+++ addon/src/main/webui/src/AppRoutes.tsx
@@ -1,36 +1,24 @@
-import { Button, Stack, Typography } from "@mui/material"
-import { Route, Routes, useNavigate } from "react-router"
+import { ThumbDown } from "@mui/icons-material"
+import { Stack, Typography } from "@mui/material"
+import { Route, Routes } from "react-router"
 
 import App from "./App"
 import { ChecksTab } from "./tabs/ChecksTab"
 
-export const AppRoutes = () => {
-    const navigate = useNavigate()
-
-    return (
-        <Routes>
-            <Route path="/" element={<App />}>
-                <Route path="/" element={<ChecksTab />} />
-                <Route
-                    path="/analysis"
-                    element={<Typography>Checks</Typography>}
-                />
-            </Route>
+export const AppRoutes = () => (
+    <Routes>
+        <Route path="*" element={<App />}>
+            <Route index element={<ChecksTab />} />
             <Route
                 path="*"
                 element={
                     <Stack textAlign="center" mt={5} spacing={5}>
                         <Typography variant="h1">
-                            Yikes! That page does not seem to exist...
-                        </Typography>
-                        <Typography>
-                            <Button onClick={() => navigate("/")}>
-                                Take me back
-                            </Button>
+                            This page has not been added yet <ThumbDown />
                         </Typography>
                     </Stack>
                 }
             />
-        </Routes>
-    )
-}
+        </Route>
+    </Routes>
+)
