a63d99e851e1a3832ebf54beb003e15804ebfce4
Tobias Rødahl Thingnes <tobbi.rt@gmail.com>
2025-04-25 14:14:26 +0200

    Hide access and community tabs

--- addon/src/main/webui/src/App.tsx
+++ addon/src/main/webui/src/App.tsx
@@ -3,8 +3,6 @@ import {
     Checklist,
     Description,
     LocalLibrary,
-    LockOpen,
-    People,
 } from "@mui/icons-material"
 import { Box, Grid, Tab, Tabs } from "@mui/material"
 import { Outlet, useLocation, useNavigate } from "react-router"
@@ -22,19 +20,21 @@ function App() {
                     onChange={(_, v) => navigate(v)}
                 >
                     <Tab label="Overview" icon={<Checklist />} value="/" />
+                    <Tab label="Logs" icon={<Description />} value="/logs" />
                     <Tab
                         label="Devices"
                         icon={<BroadcastOnHome />}
                         value="/devices"
                     />
-                    <Tab label="Logs" icon={<Description />} value="/logs" />
+                    <Tab label="Learn" icon={<LocalLibrary />} value="/learn" />
+                    {/*
                     <Tab label="Access" icon={<LockOpen />} value="/access" />
                     <Tab
                         label="Community"
                         icon={<People />}
                         value="/community"
                     />
-                    <Tab label="Learn" icon={<LocalLibrary />} value="/learn" />
+                    */}
                 </Tabs>
             </Grid>
             <Box>
